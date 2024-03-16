124b0ed50f027d706ad3f9adfd8eaf00ac805c10
Suraj Kumar Shaw <surajshaw7778@gmail.com>
2024-03-17 00:13:36 +0530

    Background Color Changer

---
+++ 15. Background Color Changer/script.js
@@ -0,0 +1,17 @@
+const btn = document.getElementById("btn");
+let hex = document.getElementById("hexCode");
+
+function randomColor() {
+    let letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+btn.addEventListener("click", () => {
+    document.body.style.backgroundColor = randomColor();
+    btn.innerHTML = `NOW COLOR IS ${randomColor()} <br> TO GET NEW COLOR <br> click agian`;
+    hex.innerHTML = randomColor();
+});
